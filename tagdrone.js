3123874ac5a4041dbffdb22b5f89960afdeb7bef
Jon Andersson <jon@goodold.se>
2012-10-05 11:06:34 +0200

    first flight

--- tagdrone.js
+++ tagdrone.js
@@ -1,7 +1,29 @@
-var clusterFinder = require("./clusterfinder");
+var clusterFinder = require("./clusterfinder"),
+    arDrone = require('ar-drone'),
+    control = arDrone.createUdpControl(),
+    png = require('png');
 
-var png = require('png');
+png.decode('./testdata/test1.png', function(pixels){
+  console.log(pixels);
+});
+var fly       = false;
+var emergency = true;
+var ref = {
+  fly: fly,
+  emergency: emergency
+}
 
-png.decode('./testdata/test1.png', function(pixels) {
-    console.log(pixels);
-     });
+setInterval(function() {
+  control.ref(ref);
+  control.pcmd();
+  control.flush();
+}, 30);
+
+// For the first second, disable emergency if there was one
+setTimeout(function() {
+  emergency = false;
+}, 1000);
+
+setTimeout(function() {
+  fly = false;
+}, 5000);
