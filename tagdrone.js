cab7ea53208c725e678403ba268e2183e6aedce0
camilo tapia <camilo.tapia@gmail.com>
2012-10-05 10:20:16 +0200

    init 2

--- tagdrone.js
+++ tagdrone.js
@@ -1,3 +1,7 @@
-var clusterFinder = require(".clusterfinder");
+var clusterFinder = require("./clusterfinder");
 
+var png = require('png');
 
+png.decode('./testdata/test1.png', function(pixels) {
+    console.log(pixels);
+     });
